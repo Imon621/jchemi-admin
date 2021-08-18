9d1da02e72641ab411badcce86dade168d8ddc8f
Imon <imon62121@gmail.com>
2021-08-18 16:54:11 +0600

    changed server

--- src/component/fireConfig.js
+++ src/component/fireConfig.js
@@ -1,6 +1,7 @@
 import firebase from "firebase";
 
 // Your web app's Firebase configuration
+/*
 var firebaseConfig = {
   apiKey: "AIzaSyB-7LeElka_nAHqWkB_PNVWhZ3gBYTrx5Y",
   authDomain: "jchemi-11e8d.firebaseapp.com",
@@ -9,6 +10,15 @@ var firebaseConfig = {
   messagingSenderId: "898389540363",
   appId: "1:898389540363:web:bdc3b642fe430e6dedd10e",
 };
+*/
+  var firebaseConfig = {
+    apiKey: "AIzaSyDkIITQs3X9FjYqExUJrpy1Bd8Lc70VBLQ",
+    authDomain: "jchemi-e63be.firebaseapp.com",
+    projectId: "jchemi-e63be",
+    storageBucket: "jchemi-e63be.appspot.com",
+    messagingSenderId: "930822034140",
+    appId: "1:930822034140:web:372b8e495d89ed488dda19"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 export default firebase;
